ea1920818ca324b6c58c2c091565585fe6d02a0b
katy <katysorourke@gmail.com>
2016-05-28 18:03:57 -0700

    Initial commit for Ink, a creative project with no plan.

---
+++ Ink/Ink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ink.Startup))]
+namespace Ink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
