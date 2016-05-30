8380be16474a2e61d3682ceaaba1db9bb189ead3
katy <katysorourke@gmail.com>
2016-05-29 19:48:08 -0700

    Organized like-elements by creating a class and referencing the class on each element. Pulled JavaScript code out of .cshtml files into .js files.

---
+++ Ink/Ink/src/Home/Calculator.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    function viewModel() {
+        var self = this;
+        self.numKnockoutA = ko.observable(0);
+        self.numKnockoutB = ko.observable(0);
+        self.sumKnockout = ko.computed(function () { return +self.numKnockoutA() + +self.numKnockoutB(); });
+    };
+
+    ko.applyBindings(new viewModel());
+});
+
+function addJs() {
+    var numA = +document.getElementById("addJsNumA").value;
+    var numB = +document.getElementById("addJsNumB").value;
+    var sum = numA + numB;
+    document.getElementById("sumJs").innerHTML = sum;
+}
---
+++ Ink/Ink/src/Home/YodaInstructor.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+
+    function viewModel() {
+        var self = this;
+        self.sentence = ko.observable("Enter a sentence, and you will learn how to 'Yoda'.");
+        self.yoda = ko.observable();
+
+        self.teach = function () {
+            $.ajax({
+                url: "https://yoda.p.mashape.com/yoda?sentence=" + self.sentence(),
+                type: "GET",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('X-Mashape-Key', 'o5CVpJ6fr9mshXVRzDciilOn6ndnp1yD6PYjsnuR1reXLhRrAT');
+                },
+                success: function (data) {
+                    self.yoda(data);
+                }
+            });
+        }
+    };
+
+    ko.applyBindings(new viewModel());
+});
